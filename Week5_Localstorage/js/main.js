863c2715139e62427ccf4fd93304e266288b76ea
kooh125 <hyunjeekoo@Hyunjee-Koos-iMac.local>
2013-10-02 22:01:56 -0400

    Week5_Localstorage

---
+++ Week5_Localstorage/js/main.js
@@ -0,0 +1,127 @@
+/**
+ * advanced:
+ * make this work offline
+ * https://developer.mozilla.org/en-US/docs/HTML/Using_the_application_cache
+ * note: you must edit the manifest to update files
+ *
+ */
+
+
+
+var posts = [];
+
+/**
+ * form action
+ *
+ */
+$('#draft button').click( function(event){
+
+    // stop form from trying to send & refresh page
+    event.preventDefault();
+
+    // create post from form
+    var post = {};
+    post.title = $('#title').val();
+    post.content = $('#content').val();
+
+    // add post to posts
+    posts.push(post);
+
+    console.log('post: ',post);
+    console.log('posts: ',posts);
+	
+    displayPost(post);
+    storePosts(posts);
+});
+
+/**
+ * display posts
+ *
+ */
+function displayPost(post){
+    var html = '<article><h2>'+ post.title +'</h2><p>'+ post.content +'</p></article>';
+    $('#feed').prepend(html);
+	changeColor();
+}
+
+
+/**
+ * store posts
+ *
+ * note: localStorage only stores STRINGS
+ *          arrays/objects must be STRINGIFIED
+ *          numbers are fine but will be returned as a strong
+ *
+ */
+
+function storePosts(posts){
+
+    console.log('array: ' + posts);
+
+    // make the array a string
+    posts = JSON.stringify(posts);
+    console.log('json: ' + posts);
+
+    // store the string
+    localStorage.posts = posts;
+
+}
+
+function changeColor() {
+	$('#feed article').click(function() {
+		$(this).addClass('active');
+		$(this).toggleClass('inactive');
+	});
+}
+
+/**
+ * localStorage = STRINGS only!!
+ *
+ * note: localStorage only stores STRINGS
+ *  - arrays/objects must be STRINGIFIED before storage, PARSED after retrieval. 
+ *  - numbers also: 
+ *       var num = localStorage.mynumber;   // '10.123' 
+ *           num = parseFloat(num);         // 10.123 
+ *           num = parseInt(num);           // 10
+ *
+ */
+
+
+/**
+ * load posts
+ *
+ * note: localStorage only stores STRINGS
+ *          arrays/objects must be PARSED
+ *          numbers also: var num = parseInt(); 
+ *
+ */
+
+function loadPosts(){
+
+    // check for posts in storage
+    if (localStorage.posts) { 
+
+        posts = localStorage.posts;
+
+        // turn string into an array
+        posts = JSON.parse(posts);
+
+        // loop thru items in the array
+        for( i=0, count=posts.length; i<count; i++ ){
+
+            var post = posts[i]
+            console.log( post );
+            displayPost(post);
+        }
+    } else { // nothing in storage?
+    
+        posts = []; 
+    
+    }
+
+}
+
+// load posts on page load
+$(function(){
+	loadPosts();	
+});
